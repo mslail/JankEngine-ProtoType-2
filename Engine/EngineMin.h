7f2dd467d32bf9c3547ce93557f641eed9ed1414
Meetinder Lail <meetinderlail@yahoo.ca>
2026-01-17 19:12:01 -0500

    Init Jank Commit

---
+++ Engine/EngineMin.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef ENGINE_EXPORTS
+
+#define ENGINE_API __declspec(dllexport)
+
+#else
+
+#define ENGINE_API __declspec(dllimport)
+
+#endif 
---
+++ Engine/TempClass.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "EngineMin.h"
+
+ class ENGINE_API  TempClasss
+{
+
+public:
+	int test = 0;
+};
+
